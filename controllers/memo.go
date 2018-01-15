da2c843acf3c4dd66caa4b906ccbea300336774f
Tecshuttle <tecshuttle@gmail.com>
2018-01-15 19:45:27 +0800

    memo可以编辑单、双引号。

--- controllers/memo.go
+++ controllers/memo.go
@@ -79,9 +79,9 @@ func (c *MemoController) SaveItem() {
 	//获取post参数
 	id := c.GetString("id", "")
 	type_id := c.GetString("type_id", "")
-	question := c.GetString("question", "")
-	answer := c.GetString("answer", "")
-	explain := c.GetString("explain", "")
+	question := Escape(c.GetString("question", ""))
+	answer := Escape(c.GetString("answer", ""))
+	explain := Escape(c.GetString("explain", ""))
 	module := c.GetString("module", "")
 	sync_state := c.GetString("sync_state", "")
 	mtime := c.GetString("mtime", "")
@@ -113,6 +113,47 @@ func (c *MemoController) SaveItem() {
 	}
 
 	c.ServeJSON()
+}
+
+func Escape(sql string) string {
+	dest := make([]byte, 0, 2*len(sql))
+	var escape byte
+	for i := 0; i < len(sql); i++ {
+		c := sql[i]
+
+		escape = 0
+
+		switch c {
+		case 0: /* Must be escaped for 'mysql' */
+			escape = '0'
+			break
+		case '\n': /* Must be escaped for logs */
+			escape = 'n'
+			break
+		case '\r':
+			escape = 'r'
+			break
+		case '\\':
+			escape = '\\'
+			break
+		case '\'':
+			escape = '\''
+			break
+		case '"': /* Better safe than sorry */
+			escape = '"'
+			break
+		case '\032': /* This gives problems on Win32 */
+			escape = 'Z'
+		}
+
+		if escape != 0 {
+			dest = append(dest, '\\', escape)
+		} else {
+			dest = append(dest, c)
+		}
+	}
+
+	return string(dest)
 }
 
 func (c *MemoController) Create() {
